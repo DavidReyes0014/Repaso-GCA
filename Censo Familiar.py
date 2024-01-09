6c242eb71bc5c4480776571ff6bfba4f2441b249
DavidReyes0014 <146682277+DavidReyes0014@users.noreply.github.com>
2024-01-08 20:07:50 -0600

    Add files via upload

---
+++ Censo Familiar.py
@@ -0,0 +1,30 @@
+#Elabore un algoritmo para realizar un Censo en un barrio
+#dicho formulario debe contener la informacion
+#del padre, madre, y dos hijos. Al igual debe preguntar el 
+#numero de bloque y casa.
+
+
+#input se utiliza para caracteres
+nombre_del_padre=int 
+nombre_de_la_madre=int
+nombre_del_primer_hijo=int
+nombre_del_segundo_hijo=int
+numero_de_bloque=int
+numero_de_casa=int
+
+#solicitud de datos
+
+nombre_del_padre=input("Ingrese el nombre del padre: ")
+nombre_de_la_madre=input("Ingrese el nombre de la madre ")
+nombre_del_primer_hijo=input("Ingrese el nombre del primer hijo: ")
+nombre_del_segundo_hijo=input("Ingrese el nombre del segundo hijo: ")
+numero_de_bloque=input("Ingrese el numero de Bloque:  ")
+numero_de_casa=input("Ingrese el nuemro de su Casa:  ")
+
+#salida de datos
+print("Nombre del padre: ", nombre_del_padre)
+print("Nombre de la madre: ", nombre_de_la_madre)
+print("Nombre del primer hijo: ", nombre_del_primer_hijo)
+print("Nombre del segundo hijo: ", nombre_del_segundo_hijo)
+print("Numero de Bloque: ", numero_de_bloque)
+print("Numero de Casa: ",numero_de_casa)
