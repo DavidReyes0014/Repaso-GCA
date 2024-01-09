6c0ce0f1612f84a72586ef5fdf68b3c28d858d77
DavidReyes0014 <146682277+DavidReyes0014@users.noreply.github.com>
2024-01-08 19:58:20 -0600

    Add files via upload

---
+++ Encuesta_Nombre_Apellido.py
@@ -0,0 +1,15 @@
+
+#Elabore una encuesta que solicite los siguientes datos, el nombre, 
+#el apellido, la edad, su lugar de residencia, el numero de identidad.
+
+
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido: ")
+edad=input("Ingrese suedad:")
+colonia=input("Ingrese su lugar de residencia: ")
+cedula=input("Ingrese su numero de identidad sin guiones: ")
+
+print("El nombre completo del usuario es:"+nombre,apellido)
+print("La edad del usuario es: "+edad)
+print("El numero de identidad del usuario es:"+cedula)
+print("Su lugar de residencia es: " +colonia)
